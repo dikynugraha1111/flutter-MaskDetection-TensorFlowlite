5e76d56c06481e45de84ee1a519ebc6be1042426
Diky <dikynugraha1111@gmail.com>
2021-05-22 16:29:43 +0700

    final code 1
    await for test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-void main() {
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(MyApp());
 }
 
@@ -18,19 +26,98 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  CameraImage cameraImage;
+  CameraController cameraController;
+  String result = "";
+
+// Intial camera. camera stream(Live).
+  initCamera() {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        cameraController.startImageStream((image) {
+          cameraImage = image;
+          runModel();
+        });
+      });
+    });
+  }
+
+// Load a model of Tflite. have 2 params, they are model and labels where before has been generated in https://teachablemachine.withgoogle.com/train/image
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "dataset/converted_tflite/model_unquant.tflite",
+        labels: "dataset/converted_tflite/labels.txt");
+  }
+
+// for run a model
+  runModel() async {
+    if (cameraImage != null) {
+      //run the model on frame because we are using the live stream of the image
+      var recognitions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage.planes.map((e) {
+            return e.bytes;
+          }).toList(),
+          imageHeight: cameraImage.height,
+          imageWidth: cameraImage.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+      recognitions.forEach((element) {
+        setState(() {
+          result = element["label"];
+          print(result);
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initCamera();
+    loadModel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              height: MediaQuery.of(context).size.height - 170,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController.value.isInitialized
+                  ? Container()
+                  : AspectRatio(
+                      aspectRatio: cameraController.value.aspectRatio,
+                      child: CameraPreview(cameraController),
+                    ),
+            ),
+          ),
+          Text(
+            result,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          )
+        ],
       ),
     );
   }
